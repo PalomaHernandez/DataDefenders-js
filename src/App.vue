5c61e1e9573c2897cdd29fc65c208d74b131cb4c
Zeke <erb1995@gmail.com>
2023-05-24 17:06:09 -0300

    Bain-storming updates.
    - UI.
    - Config.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
 	<main class="flex">
-		<aside class="h-screen w-fit flex flex-col bg-sky-700">
+		<aside class="h-screen w-fit flex flex-col bg-sky-900">
 			<div class="flex justify-center p-4">
 				<img alt="Vue logo" class="h-12" src="@/assets/logos/um-white.svg">
 			</div>
 			<nav class="main-nav flex-grow overflow-y-auto">
 				<RouterLink :to="{name: 'home'}">
 					<i class="fa-solid fa-home"></i>
+				</RouterLink>
+				<RouterLink :to="{name: 'home'}">
+					<i class="fa-solid fa-search"></i>
+				</RouterLink>
+				<RouterLink :to="{name: 'home'}">
+					<i class="fa-solid fa-file-edit"></i>
+				</RouterLink>
+				<RouterLink :to="{name: 'home'}">
+					<i class="fa-solid fa-user-circle"></i>
 				</RouterLink>
 			</nav>
 		</aside>
@@ -15,7 +24,13 @@
 				<div class="flex-grow">
 					<slot name="title"/>
 				</div>
-				<slot name="status"/>
+				<div class="fixed lg:relative left-0 bottom-14 lg:left-auto lg:bottom-auto p-5 lg:p-0 z-20 w-full lg:w-auto">
+					<div class="relative w-full lg:w-auto">
+						<div class="absolute lg:relative w-full lg:w-auto">
+							<slot name="status"/>
+						</div>
+					</div>
+				</div>
 				<slot name="tools"/>
 			</header>
 			<section class="flex-grow overflow-y-auto bg-gray-50">
